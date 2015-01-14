ce94559bfc9cec247426e462feab8ff4cc5542b2
Marko Back <marko.back@tieto.com>
2015-01-14 11:52:58 +0200

    eka kommitti

---
+++ src/juha/hellooworld.java
@@ -0,0 +1,13 @@
+package juha;
+
+public class hellooworld {
+
+    public static void main(String[] args) {
+        int luku;
+        luku = 1;
+        System.out.println("jippii");
+        // TODO Auto-generated method stub
+
+    }
+
+}
